2e022f181b80dd27a45a4caff91d037dea4311ca
azikenp <azikenp26@gmail.com>
2024-10-21 11:57:44 +0100

    charts

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,9 +1,45 @@
+import { BarChart } from "@mui/x-charts/BarChart";
+
 const Chart = () => {
   return (
     <div className="flex-1 px-5">
       <div>
-        <h3 className="text-[#000000] font-medium text-[17px] leading-3">Event Registrations per month</h3>
-        <div>Chart</div>
+        <h3 className="text-[#000000] font-medium text-[17px] leading-3">
+          Event Registrations per month
+        </h3>
+        <div>
+          <BarChart
+            xAxis={[
+              {
+                id: "barCategories",
+                data: [
+                  "Jn",
+                  "Fb",
+                  "Mr",
+                  "Ap",
+                  "Ma",
+                  "Ju",
+                  "Jl",
+                  "Au",
+                  "Se",
+                  "Oc",
+                  "No",
+                  "De",
+                ],
+                scaleType: "band",
+              },
+            ]}
+            series={[
+              {
+                data: [
+                  650, 950, 780, 420, 1000, 580, 860, 380, 850, 650, 980, 600,
+                ],
+              },
+            ]}
+            width={500}
+            height={262}
+          />
+        </div>
       </div>
     </div>
   );
